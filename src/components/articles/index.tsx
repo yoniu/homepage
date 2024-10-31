ec1629591a60a8956205116bbc0d25199b2892ec
xu_jingzhi <xu_jingzhi@200011.net>
2024-10-31 09:14:09 +0000

    feat: 新增获取 hashnode 文章列表

---
+++ src/components/articles/index.tsx
@@ -0,0 +1,63 @@
+import { useQuery } from 'graphql-hooks'
+import dayjs from "dayjs"
+
+interface IPost {
+  id: string;
+  title: string;
+  url: string;
+  publishedAt: string;
+}
+
+const POSTS_QUERY = `query Publication {
+  publication(host: "blog.200011.net") {
+      isTeam
+      title
+      posts(first: 5) {
+          edges {
+              node {
+                  id
+                  title
+                  url
+                  publishedAt
+              }
+          }
+      }
+  }
+}`
+
+export default function Articles() {
+  const { loading, error, data } = useQuery(POSTS_QUERY)
+
+  if (error) return 'Something Bad Happened'
+
+  const formatDay = (date: string) => {
+    return dayjs(date).format('MMM D, YYYY')
+  }
+
+  return (
+    <div className="space-y-1">
+      <h4 className="text-sm font-bold leading-none mb-2">Articles 📚</h4>
+      {
+        loading && !error && 
+        <div className="text-xs animate-pulse">loading...</div>
+      }
+      {
+        !loading && error && 
+        <div className="text-xs animate-pulse">Something Bad Happened.</div>
+      }
+      {
+        !loading &&
+        data.publication.posts.edges.map(({ node }: { node: IPost }) => {
+          return (
+            <div key={node.id} className="group/article flex items-center justify-between space-x-2">
+              <a className="text-xs text-black-600 rounded group-hover/article:text-white group-hover/article:bg-black px-0 py-1 group-hover/article:px-2 transition-all" href={node.url} target="_blank">{node.title}</a>
+              <span className="text-xs text-gray-500 group-hover/article:text-gray-200">
+                {formatDay(node.publishedAt)}
+              </span>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { GraphQLClient, ClientContext } from 'graphql-hooks'
+
 import App from './App';
 
 import '@/src/styles/common.less'
 import '@/styles/globals.less'
+
+const client = new GraphQLClient({
+  url: 'https://gql.hashnode.com'
+})
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <App />
+      <ClientContext.Provider value={client}>
+        <App />
+      </ClientContext.Provider>
     </React.StrictMode>,
   );
 }
