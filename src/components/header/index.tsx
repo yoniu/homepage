454935442a08dc3982f9d593147c5d772316a96a
xu_jingzhi <xu_jingzhi@200011.net>
2024-10-31 06:10:42 +0000

    feat: 头像新增音乐播放功能

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,5 @@
+import Avatar from "@/src/components/avatar"
+
 export default function Header() {
   return (
     <div className="flex items-end w-full space-y-1">
@@ -7,7 +9,7 @@ export default function Header() {
           欢迎来到我的个人主页
         </p>
       </div>
-      <img className="w-10 rounded" src="https://p2.music.126.net/cPyfIo_ZV6lfQnZa7J-HOg==/109951165991680568.jpg" alt="yoniu" />
+      <Avatar />
     </div>
   )
 }
