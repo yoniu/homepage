8ad03079d1873296b2b49e07c02affd61afe4155
xu_jingzhi <xu_jingzhi@200011.net>
2024-11-01 05:14:40 +0000

    fixed: 修复构建报错

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -13,7 +13,7 @@ export default function Avatar() {
   const mp3 = "https://freetyst.nf.migu.cn/public%2Fproduct9th%2Fproduct45%2F2022%2F08%2F0520%2F2018%E5%B9%B411%E6%9C%8807%E6%97%A515%E7%82%B910%E5%88%86%E6%89%B9%E9%87%8F%E9%A1%B9%E7%9B%AESONY100%E9%A6%96-2%2F%E5%85%A8%E6%9B%B2%E8%AF%95%E5%90%AC%2FMp3_64_22_16%2F6005970YTPX204044.mp3?Key=c3c46990dc4ac321&Tim=1730354565790&channelid=01&msisdn=25955c9422344dc7abe5a4bf2a4318b9"
   const playBtnClass = "text-white opacity-0 group-hover/item:opacity-100 transition-all translate-y-2 group-hover/item:translate-y-0 rounded-full"
 
-  const soundRef = useRef(null);
+  const soundRef = useRef<Howl | null>(null);
 
   useEffect(() => {
     soundRef.current = new Howl({
@@ -28,17 +28,19 @@ export default function Avatar() {
     });
 
     return () => {
-      soundRef.current && (soundRef.current as any)?.stop();
+      if (soundRef.current) {
+        soundRef.current?.stop()
+      }
     };
   }, []);
 
   const handlePlay = () => {
     if (loading) return
     if (!playing) {
-      ; (soundRef.current as any)?.play();
+      soundRef.current?.play();
       setPlay(true)
     } else {
-      ; (soundRef.current as any)?.pause();
+      soundRef.current?.pause();
       setPlay(false)
     }
   }
