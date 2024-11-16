ad3f3520a5c1923cd3109162ca0af919a62d9bdb
yoniu <1376652515@qq.com>
2024-11-16 15:49:12 +0800

    feat:  编辑器新增通过 query 获取 id

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -3,15 +3,39 @@
 import AdminSidebar from '@/src/components/sidebar/admin';
 import { StateProvider as PlayerStateProvider } from '@/src/stores/audio';
 import { StateProvider as UserStateProvider } from '@/src/stores/user';
+import { App } from 'antd';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
-export default async function Page() {
+export default function Page() {
+
+  const router = useRouter()
+  const query = useSearchParams()
+  const { modal } = App.useApp()
+
+  const id  = query.get('id')
+
+  // 守卫：如果无 ID 则弹窗是否创建
+  useEffect(() => {
+    if (query.get('id') === null) {
+      modal.confirm({
+        title: 'No Id Number',
+        content: "Create A New Moment?",
+        closable: true,
+        onOk: () => {},
+        onCancel: () => {
+          router.replace('/admin')
+        }
+      })
+    }
+  }, [])
 
   return (
     <UserStateProvider>
       <PlayerStateProvider>
         <div id="main">
           <div className="relative flex flex-col w-full h-full">
-            editor {}
+            editor {id ?? 'no id'}
           </div>
         </div>
         <div id="sidebar">
