42aa4d9b96cf624af02eff2b08082d21f73ad836
yoniu <1376652515@qq.com>
2024-11-17 20:40:14 +0800

    feat: 编辑器新增背景音乐编辑

--- src/components/editor/text/sidebar.tsx
+++ src/components/editor/text/sidebar.tsx
@@ -1,9 +1,11 @@
 import Upload from "@/src/components/editor/upload";
+import EditorMusic from "@/src/components/editor/music";
 
 export default function TextEditorSidebar() {
   return (
     <>
       <Upload />
+      <EditorMusic />
     </>
   )
 }
