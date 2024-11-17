32dd93a064e69bb1ff56a8bbff369348ea3c9748
yoniu <1376652515@qq.com>
2024-11-18 00:52:17 +0800

    feat: text item 新增 photosets 显示

--- src/components/moments/item/text.tsx
+++ src/components/moments/item/text.tsx
@@ -3,10 +3,11 @@ import { IMusicItem } from "@/src/components/editor/music";
 import MomentControl from "@/src/components/moments/control";
 import CONST from "@/src/configs/consts";
 import markdownit from 'markdown-it';
+import { Col, Image, Row } from "antd";
 
 export interface ITextItem {
   music?: IMusicItem
-  photosets?: IPhotosetItem
+  photosets?: IPhotosetItem[]
 }
 
 interface IProps {
@@ -34,6 +35,30 @@ export default function TextItem({ item }: IProps) {
     }
   }
 
+  const Photosets = () => {
+    if (item.attributes && item.attributes.photosets) {
+      return (
+        <Row className="mx-0 mt-2" gutter={[6, 6]}>
+          {
+            item.attributes.photosets?.map((photo) => (
+              <Col key={item.id} span={8}>
+                <Image
+                  wrapperClassName="relative aspect-square w-full overflow-hidden rounded"
+                  className="absolute w-full !h-full object-cover"
+                  src={photo.url}
+                  alt={photo.name}
+                />
+              </Col>
+            ))
+          }
+        </Row>
+      )
+    }
+    return (
+      <></>
+    )
+  }
+
   return (
     <div className="flex flex-col shadow-lg rounded-md overflow-hidden w-full h-full border">
       <Music />
@@ -41,8 +66,8 @@ export default function TextItem({ item }: IProps) {
         <div className="absolute top-0 left-0 w-full h-full p-3 overflow-y-auto overflow-x-hidden">
           { item.title && <h3 className="text-xl font-bold mb-2">{item.title}</h3> }
           { item.content && <div dangerouslySetInnerHTML={{ __html: md.render(item.content) }} /> }
+          <Photosets />
         </div>
-        {/* todo: 显示 photosets */}
         <MomentControl />
       </div>
     </div>
