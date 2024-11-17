90bce8077a6b1a2daebe8b500ec0e0af95e9cf1d
yoniu <1376652515@qq.com>
2024-11-17 17:41:12 +0800

    feat: 新增首页 moment 上下一条控制

--- src/components/moments/item/loading.tsx
+++ src/components/moments/item/loading.tsx
@@ -1,3 +1,5 @@
+import MomentControl from "../control";
+
 export default function MomentLoading() {
   return (
     <div className="flex flex-col shadow-lg rounded-md overflow-hidden w-full h-full border">
@@ -11,6 +13,7 @@ export default function MomentLoading() {
       <div className="relative w-full bg-gray-300 animate-pulse flex-1 flex items-center justify-center">
         <span className="absolute text-sm bg-orange-400 py-1 px-2 text-white left-0 top-0 translate-x-6 translate-y-9">comming soon</span>
         <span className="absolute text-sm bg-orange-400 py-1 px-2 text-white left-0 top-0 translate-x-32 translate-y-80">抖音小红书常驻用户罢了</span>
+        <MomentControl />
       </div>
     </div>
   )
