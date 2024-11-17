e7d44a8fd27ef7b06a1c731927bb0af947388ced
yoniu <1376652515@qq.com>
2024-11-18 00:22:57 +0800

    feat: 新增首页获取文章列表 && 展示 text item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,76 @@
 import MomentLoading from '@/src/components/moments/item/loading';
 
 import Sidebar from "@/src/components/sidebar"
+import { useStateContext as useMomentStateContext } from '@/src/stores/moment';
+import { useEffect, useMemo, useState } from 'react';
+import api from '@/src/utils/api';
+import { App, Spin } from 'antd';
+import { TResponseError } from '@/src/utils/axiosInstance';
+import TextItem from '../components/moments/item/text';
 
 export default function Page() {
 
+  const { state, dispatch }  = useMomentStateContext();
+  const { message: messageApi } = App.useApp()
+
+  const [loading, setLoading] = useState(true);
+
+  const displayer: Record<EMomentType, JSX.Element> = {
+    text: <TextItem item={state.momentList[state.currentIndex]} />,
+    image: <div>image</div>,
+    video: <div>video</div>,
+    live: <div>live</div>,
+  }
+
+  useEffect(() => {
+    handleGetPublicAll()
+  }, [])
+
+  const handleGetPublicAll = () => {
+    setLoading(true)
+    getPublicAll(state.page, state.pageSize).then(res => {
+      const { hasNextPage, moments } = res.data
+      // 如果当前分页大于 1 则推入栈，否则直接赋值
+      if (state.page) {
+        dispatch({
+          type: "UPDATELIST",
+          momentList: [...state.momentList, ...moments],
+          page: state.page + 1,
+          hasNextPage
+        })
+      } else {
+        dispatch({
+          type: "UPDATELIST",
+          momentList: [...res.data.moments],
+          page: state.page + 1,
+          hasNextPage
+        })
+      }
+    }).catch(err => {
+      const { message } = err as TResponseError
+      if (Array.isArray(message)) {
+        message.map((msg) => messageApi.error(msg))
+      } else {
+        messageApi.error(message)
+      }
+    }).finally(() => {
+      setLoading(false)
+    })
+  }
+
+  const currentMomentType = useMemo<EMomentType>(() => {
+    if (state.momentList[state.currentIndex] && state.momentList[state.currentIndex].attributes && state.momentList[state.currentIndex].attributes.type) {
+      return state.momentList[state.currentIndex].attributes.type
+    }
+    return 'text'
+  }, [state.momentList[state.currentIndex]])
+
   return (
     <>
+      <Spin spinning={loading} fullscreen={true} />
       <div id="main">
         <div className="flex items-center justify-center w-full h-full">
-          <MomentLoading />
+          { !state.momentList.length ? <MomentLoading /> : displayer[currentMomentType] }
         </div>
       </div>
       <div id="sidebar">
@@ -18,4 +80,8 @@ export default function Page() {
       </div>
     </>
   )
+}
+
+function getPublicAll(page: number, pageSize: number) {
+  return api.get<IGetMomentListResponse>('/moment/public', { page, pageSize });
 }
--- src/components/moments/item/loading.tsx
+++ src/components/moments/item/loading.tsx
@@ -11,8 +11,8 @@ export default function MomentLoading() {
         <div className="w-16 h-6 bg-gray-300 rounded animate-pulse"></div>
       </div>
       <div className="relative w-full bg-gray-300 animate-pulse flex-1 flex items-center justify-center">
-        <span className="absolute text-sm bg-orange-400 py-1 px-2 text-white left-0 top-0 translate-x-6 translate-y-9">comming soon</span>
-        <span className="absolute text-sm bg-orange-400 py-1 px-2 text-white left-0 top-0 translate-x-32 translate-y-80">抖音小红书常驻用户罢了</span>
+        {/* <span className="absolute text-sm bg-orange-400 py-1 px-2 text-white left-0 top-0 translate-x-6 translate-y-9">comming soon</span>
+        <span className="absolute text-sm bg-orange-400 py-1 px-2 text-white left-0 top-0 translate-x-32 translate-y-80">抖音小红书常驻用户罢了</span> */}
         <MomentControl />
       </div>
     </div>
