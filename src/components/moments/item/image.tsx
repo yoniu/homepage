28d3dab9e05b498306e427bfd99f81bebaed88d6
yoniu <1376652515@qq.com>
2024-11-19 01:17:31 +0800

    fixed: 修复 Image item 切换图片不切换背景

--- src/components/moments/item/image.tsx
+++ src/components/moments/item/image.tsx
@@ -2,7 +2,7 @@ import { IMusicItem } from "@/src/components/editor/music";
 import MomentControl from "@/src/components/moments/control";
 import { Carousel } from "antd";
 import MusicPlayer from "@/src/components/play/music";
-import { memo, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export interface IImageItem {
   music?: IMusicItem
@@ -31,40 +31,31 @@ export default function ImageItem({ item }: IProps) {
     }
   }, [item.attributes])
 
-  const Photosets = memo(() => {
-    if (item.attributes && item.attributes.photosets) {
-      return (
-        <Carousel
-         rootClassName="w-full"
-         dotPosition="bottom"
-         adaptiveHeight={true}
-         autoplay={true}
-         autoplaySpeed={2000}
-         afterChange={handleCarouselChange}
-        >
-          {
-            (item.attributes.photosets as IPhotosetItem[]).map((photoset) => (
-              <div className="h-full flex justify-center items-center" key={photoset.id}>
-                <img src={photoset.url} alt={photoset.name} className="w-full h-full object-contain" />
-              </div>
-            ))
-          }
-        </Carousel>
-      )
-    }
-    return (
-      <></>
-    )
-  })
-  Photosets.displayName = "Photosets";
-
   return (
     <div className="flex flex-col shadow-lg rounded-md overflow-hidden w-full h-full border">
       {(item && item.attributes && item.attributes.music) ? <MusicPlayer {...item.attributes.music} /> : null }
       <div className="relative w-full h-full flex-1 bg-white">
         <div className="absolute top-0 left-0 w-full h-full overflow-hidden flex items-center justify-center">
           { bg ? <img src={bg} alt="background" className="absolute w-full h-full object-cover transform scale-125 blur" /> : null }
-          <Photosets />
+          {
+            (item.attributes && item.attributes.photosets) &&
+              <Carousel
+              rootClassName="w-full"
+              dotPosition="bottom"
+              adaptiveHeight={true}
+              autoplay={true}
+              autoplaySpeed={2000}
+              afterChange={handleCarouselChange}
+            >
+              {
+                (item.attributes.photosets as IPhotosetItem[]).map((photoset) => (
+                  <div className="h-full flex justify-center items-center" key={photoset.id}>
+                    <img src={photoset.url} alt={photoset.name} className="w-full h-full object-contain" />
+                  </div>
+                ))
+              }
+            </Carousel>
+          }
         </div>
         <MomentControl />
       </div>
