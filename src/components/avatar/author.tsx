2029a3fdf16970cfbeac5313054db40650f2d9a4
yoniu <1376652515@qq.com>
2024-11-18 21:58:19 +0800

    feat: gravatar 改用 weavatar

--- src/components/avatar/author.tsx
+++ src/components/avatar/author.tsx
@@ -13,7 +13,7 @@ export default function AuthorAvatar({ email, name, other }: IProps) {
 
   return (
     <div className="flex items-center space-x-2 leading-4 mb-2 py-1 pl-1 pr-2 bg-gray-50 rounded-md">
-      <img className="w-6 rounded " src={`https://www.gravatar.com/avatar/${hash}?s=100`} alt={name} />
+      <img className="w-6 rounded " src={`https://weavatar.com/avatar/${hash}?s=100`} alt={name} />
       <div className="flex-1 text-sm font-bold text-gray-600">{name}</div>
       <div className="justify-self-end text-xs text-gray-500">{other}</div>
     </div>
