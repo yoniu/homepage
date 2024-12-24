d4449442a8d827df1089a6f7f44f11a5ebfde025
yoniu <1376652515@qq.com>
2024-12-25 00:26:49 +0800

    feat: 新增视频组件禁音

--- src/components/play/video.tsx
+++ src/components/play/video.tsx
@@ -1,3 +1,4 @@
+import { MutedFilled, MutedOutlined } from '@ant-design/icons';
 import { PauseIcon, PlayIcon } from '@radix-ui/react-icons';
 import { useState } from 'react';
 import ReactPlayer from 'react-player';
@@ -5,26 +6,41 @@ import ReactPlayer from 'react-player';
 export default function VideoPlayer({ url, autoPlay = false }: { url: string, autoPlay?: boolean }) {
 
   const [playing, setPlaying] = useState(autoPlay)
+  const [muted, setMuted] = useState(true)
 
   return (
     <div className="absolute top-0 left-0 bottom-0 right-0 group/video">
       <ReactPlayer
         url={url}
         playing={playing}
+        muted={muted}
         className="absolute top-0 left-0 bottom-0 right-0"
         width="100%"
         height="100%"
+        loop
       />
-      <button
-        className="text-white/50 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 group-hover/video:opacity-100 opacity-0 transition-opacity"
-        onClick={() => setPlaying(!playing)}
-      >
-        {
-          playing ?
-          <PauseIcon width="36" height="36" /> :
-          <PlayIcon width="36" height="36" />
-        }
-      </button>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center space-x-2">
+        <button
+          className="text-white/80 group-hover/video:opacity-100 opacity-0 transition-opacity"
+          onClick={() => setPlaying(!playing)}
+        >
+          {
+            playing ?
+            <PauseIcon width="36" height="36" /> :
+            <PlayIcon width="36" height="36" />
+          }
+        </button>
+        <button
+          className="text-white/80 group-hover/video:opacity-100 opacity-0 transition-opacity"
+          onClick={() => setMuted(!muted)}
+        >
+          {
+            muted ?
+            <MutedFilled className="text-[36px]" width="36" height="36" /> :
+            <MutedOutlined className="text-[36px]" width="36" height="36" />
+          }
+        </button>
+      </div>
     </div>
   );
 }
