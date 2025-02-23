9c84f2d9a8456063aa47b4aeed36237e71d864b9
yoniu <1376652515@qq.com>
2025-02-23 16:27:48 +0800

    feat: 新增 moment state context 设置列表显示类型

--- src/stores/moment.tsx
+++ src/stores/moment.tsx
@@ -12,6 +12,7 @@ interface IState {
   pageSize: number;
   hasNextPage: boolean;
   momentList: IMomentItem<any>[];
+  displayType: "tiktok" | "masonry";
 }
 
 // 定义初始状态
@@ -19,16 +20,19 @@ const initialState: IState = {
   loading: false,
   currentIndex: 0,
   page: 1,
-  pageSize: 5,
+  pageSize: 6,
   hasNextPage: false,
   momentList: [],
+  displayType: "tiktok",
 };
 
 type TAction = 
   | { type: 'UPDATELIST', momentList: IMomentItem<any>[], page: number, hasNextPage: boolean } // 更新 list
   | { type: 'PREVINDEX' } // 上一条
   | { type: 'NEXTINDEX' } // 下一条
+  | { type: 'SETINDEX', index: number } // 切换 index
   | { type: 'SETLOADING', state: boolean } // Loading
+  | { type: 'SETDISPLAYTYPE', displayType: "tiktok" | "masonry" } // 切换显示类型
 
 // 定义 reducer 函数
 const reducer = (state: IState, action: TAction): IState => {
@@ -45,8 +49,13 @@ const reducer = (state: IState, action: TAction): IState => {
         return { ...state, currentIndex: state.currentIndex + 1 };
       else
         return state;
+    case 'SETINDEX':
+      console.log(action.index)
+      return {...state, currentIndex: action.index };
     case 'SETLOADING':
       return { ...state, loading: action.state };
+    case 'SETDISPLAYTYPE':
+      return {...state, displayType: action.displayType, currentIndex: 0 };
     default:
       return state;
   }
