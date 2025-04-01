a2b9b34708534500f264410390538750473520f3
yoniu <1376652515@qq.com>
2025-04-01 13:54:24 +0800

    feat: 瀑布流列表 TextItem 颜色展示

--- src/components/moments/item/masonry/text.tsx
+++ src/components/moments/item/masonry/text.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link"
 import { Fragment, useMemo } from "react"
 
+import { TTextBackgroundColor } from "@/src/components/editor/backgroundColor"
+
 export interface ITextItem {
   photosets?: IPhotosetItem[]
+  backgroundColor?: TTextBackgroundColor
 }
 
 interface IProps {
@@ -14,12 +17,36 @@ export default function MasonryTextItem({ item }: IProps) {
   const href = useMemo(() => {
     return `/moment/?id=${item.id}`
   }, [item.id])
+  
+  
+  const backgroundColor = useMemo<TTextBackgroundColor | undefined>(() => {
+    if (item.attributes?.backgroundColor) {
+      return item.attributes.backgroundColor
+    }
+  }, [item.attributes])
+
+  const textColor = useMemo(() => {
+    return backgroundColor?.textColor ?? '#333'
+  }, [backgroundColor])
+
+  const Background = () => {
+    switch (backgroundColor?.type) {
+      case 'solid':
+        return <div className="absolute bottom-0 left-0 w-full h-full inset-0 z-0" style={{ backgroundColor: backgroundColor.color }}></div>
+      case 'gradient':
+        return <div className="absolute bottom-0 left-0 w-full h-full inset-0 z-0" style={{ background: `linear-gradient(to bottom, ${backgroundColor.colors?.join(',')})` }}></div>
+      case 'image':
+        return <div className="absolute bottom-0 left-0 w-full h-full inset-0 z-0 opacity-80 scale-110 blur-[2px]" style={{ backgroundImage: `url(${backgroundColor.image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+      default:
+        return <div className="absolute bottom-0 left-0 w-full h-full inset-0 bg-gradient-to-t from-black/5 to-transparent z-0"></div>
+    }
+  }
 
   return (
     <Link className="flex flex-col rounded-md overflow-hidden border mb-4" href={href}>
       <div className="relative w-full pb-[100%] sm:pb-[133.33%] overflow-hidden">
         <div className="absolute flex items-center justify-center inset-0 p-4 overflow-hidden">
-          <div className="relative line-clamp-[9] break-words text-2xl sm:text-lg overflow-hidden text-ellipsis z-10">
+          <div className="relative line-clamp-[9] break-words text-2xl sm:text-lg overflow-hidden text-ellipsis z-10" style={{ color: textColor }}>
             { 
               item.content && item.content.split('\n').map((line, index) => (
                 <Fragment key={index}>
@@ -28,7 +55,7 @@ export default function MasonryTextItem({ item }: IProps) {
               ))
             }
           </div>
-          <div className="absolute bottom-0 left-0 w-full h-full inset-0 bg-gradient-to-t from-black/5 to-transparent z-0"></div>
+          <Background />
         </div>
       </div>
       <div className="py-2 px-4 text-md text-gray-500">
