b2dc9bc553c4d57c1249396d0fd17bdfc5e316c1
yoniu <1376652515@qq.com>
2025-04-02 12:35:47 +0800

    fixed: 修复 LA undefined

--- src/components/analyze/V6.tsx
+++ src/components/analyze/V6.tsx
@@ -3,8 +3,8 @@ import Script from "next/script";
 export default function V6Analyze() {
   return (
     <>
-      <Script charSet="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js"></Script>
-      <Script>
+      <Script charSet="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js" strategy="lazyOnload"></Script>
+      <Script strategy="lazyOnload">
         {`
           LA.init({id:"${process.env.NEXT_PUBLIC_V6_ID as string}",ck:"${process.env.NEXT_PUBLIC_V6_CK as string}",autoTrack:true,hashMode:true})
         `}
