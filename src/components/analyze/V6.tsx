c9c9255815ffff86ff1325f0f2527874a2369850
yoniu <1376652515@qq.com>
2025-04-02 12:51:36 +0800

    fixed: 修复 LA undefined 3

--- src/components/analyze/V6.tsx
+++ src/components/analyze/V6.tsx
@@ -1,14 +1,21 @@
+"use client";
+
 import Script from "next/script";
 
 export default function V6Analyze() {
   return (
     <>
-      <Script charSet="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js" strategy="lazyOnload"></Script>
-      <Script strategy="lazyOnload">
+      <Script charSet="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js"></Script>
+      <Script>
         {`
-          Promise.resolve().then(() => {
-            LA.init({id:"${process.env.NEXT_PUBLIC_V6_ID as string}",ck:"${process.env.NEXT_PUBLIC_V6_CK as string}",autoTrack:true,hashMode:true})
-          })
+          function initLA() {
+            if (typeof LA !== 'undefined') {
+              LA.init({id:"${process.env.NEXT_PUBLIC_V6_ID as string}",ck:"${process.env.NEXT_PUBLIC_V6_CK as string}",autoTrack:true,hashMode:true})
+            } else {
+              setTimeout(initLA, 100);
+            }
+          }
+          initLA();
         `}
       </Script>
     </>
